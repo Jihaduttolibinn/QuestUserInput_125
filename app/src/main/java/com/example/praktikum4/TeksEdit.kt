b73b4401e085b439cab655f3f8647502ab69042c
jihaduttolibinn <jihaduttolibin02@gmail.com>
2025-10-30 15:28:43 +0700

    Menambahankan Column

--- app/src/main/java/com/example/praktikum4/TeksEdit.kt
+++ app/src/main/java/com/example/praktikum4/TeksEdit.kt
@@ -1,11 +1,16 @@
 package com.example.praktikum4
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 
 @Composable
 
@@ -22,6 +27,14 @@ fun FormDataDiri(Modifier: Modifier
     var jenis by remember { mutableStateOf("") }
 
     val gender: List<String> = listOf("Laki-Laki", "Perempuan")
+
+    Column(
+        modifier = Modifier.padding(top = 50.dp),
+        verticalArrangement = Arrangement.Top,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ){
+
+    }
 
 
 
