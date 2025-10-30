5f689ec8513691dac401b5679147bc697ecf2ecc
jihaduttolibinn <jihaduttolibin02@gmail.com>
2025-10-30 15:18:39 +0700

    Menambahankan textJK di bagian FormDataDiri

--- app/src/main/java/com/example/praktikum4/TeksEdit.kt
+++ app/src/main/java/com/example/praktikum4/TeksEdit.kt
@@ -3,6 +3,7 @@ package com.example.praktikum4
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 
 @Composable
@@ -12,6 +13,7 @@ fun FormDataDiri(Modifier: Modifier
     //variable-variable untuk mengingat nilai masukan dari keyboard
     var textNama by remember { mutableStateOf(value = "") }
     var textAlamat by remember { mutableStateOf(value = "") }
+    var textJK by remember { mutableStateOf("") }
 
 
 }
