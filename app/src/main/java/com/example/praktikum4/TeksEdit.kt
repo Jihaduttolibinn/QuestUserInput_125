66982e2aa687b40e08731d15d52c96579db12820
jihaduttolibinn <jihaduttolibin02@gmail.com>
2025-10-30 15:16:31 +0700

    Menambahankan textNama di bagian FormDataDiri

--- app/src/main/java/com/example/praktikum4/TeksEdit.kt
+++ app/src/main/java/com/example/praktikum4/TeksEdit.kt
@@ -1,2 +1,16 @@
 package com.example.praktikum4
 
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+
+@Composable
+
+fun FormDataDiri(Modifier: Modifier
+){
+    //variable-variable untuk mengingat nilai masukan dari keyboard
+    var textNama by remember { mutableStateOf(value = "") }
+
+
+}
